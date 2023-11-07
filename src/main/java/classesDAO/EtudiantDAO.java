6cc471c83b426e23f0b128cbf89df183d884d745
yani.lhadj <lhadjy46@gmail.com>
2023-11-07 19:01:47 +0100

    Fonction test de recherche d'étudiant selon son id

--- src/main/java/classesDAO/EtudiantDAO.java
+++ src/main/java/classesDAO/EtudiantDAO.java
@@ -1,49 +1,50 @@
 package classesDAO;
-
-
 import java.sql.*;
+
+import classes.Etudiant;
 
 public class EtudiantDAO {
     // JDBC URL, username, and password of MySQL server
     static final String JDBC_URL = "jdbc:mysql://localhost:3306/Projet";
     static final String DB_USER = "root";
     static final String DB_PASSWORD = "Ff1932512210*";
-
-    public static void main(String[] args) {
-        try (Connection connection = DriverManager.getConnection(JDBC_URL, DB_USER, DB_PASSWORD)) {
+    Etudiant etudiant;
+    
+    
+    public static void rechercheEtudiantId(int id) {
+    	try (Connection connection = DriverManager.getConnection(JDBC_URL, DB_USER, DB_PASSWORD)) {
             System.out.println("Connected to the database.");
 
             // Retrieving data from Etudiant table
-            System.out.println("Etudiant Table:");
             try (Statement statement = connection.createStatement()) {
-                ResultSet etudiantResultSet = statement.executeQuery("SELECT * FROM Etudiant");
-
-                while (etudiantResultSet.next()) {
-                    int idEtudiant = etudiantResultSet.getInt("idEtudiant");
-                    String nom = etudiantResultSet.getString("nom");
-                    String prenom = etudiantResultSet.getString("prenom");
-                    int idFormation = etudiantResultSet.getInt("idFormation");
-
-                    System.out.println(
-                            "ID: " + idEtudiant + ", Nom: " + nom + ", Prenom: " + prenom + ", ID Formation: " + idFormation);
+            	
+                ResultSet etudiantResultSet = statement.executeQuery("SELECT * FROM Etudiant");    
+                int idEtudiant=0;
+                String nom="";
+                String prenom="";
+                
+                while(etudiantResultSet.next() ){
+                    idEtudiant = etudiantResultSet.getInt("idEtudiant");
+                    nom = etudiantResultSet.getString("nom");
+                    prenom = etudiantResultSet.getString("prenom");
+                    if(idEtudiant == id) {
+                    	break; 
+                    }
+                }
+                if(idEtudiant ==0) {
+                	System.out.println("Pas d'étudiant avec le numéro étudiant : "+id);
+                }else {
+                	System.out.println(nom+" "+prenom+" "+idEtudiant);
                 }
             }
-
-            // Retrieving data from Formation table
-            System.out.println("\nFormation Table:");
-            try (Statement statement = connection.createStatement()) {
-                ResultSet formationResultSet = statement.executeQuery("SELECT * FROM Formation");
-
-                while (formationResultSet.next()) {
-                    int id = formationResultSet.getInt("idFormation");
-                    String nom = formationResultSet.getString("nomFormation");
-                    String promotion = formationResultSet.getString("promotion");
-
-                    System.out.println("ID: " + id + ", Nom: " + nom + ", Promotion: " + promotion);
-                }
-            }
-        } catch (SQLException e) {
+                
+            
+    	} catch (SQLException e) {
             System.err.println("Error: " + e.getMessage());
-        }
+    	}
+    }
+    
+    public static void main(String[] args) {
+    	EtudiantDAO.rechercheEtudiantId(1);
     }
 }
